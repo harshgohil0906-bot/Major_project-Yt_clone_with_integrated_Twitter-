9ce0f0aa2c5411378b7bba6d944ed35011084dc6
Harsh Gohil <gohilharsh2006@gmail.com>
2026-06-15 12:42:46 +0530

    file uploading through cloudinary & learn about middleware(multer)

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,12 @@
+// multer --> is a middleware(jaate jaate merko milte jana)
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/temp')
+  },
+  filename: function (req, file, cb) {
+
+    cb(null, file.originalname)
+  }
+})
+
+export const upload = multer({ storage })
